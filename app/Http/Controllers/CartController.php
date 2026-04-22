722b37ccbdf272ce832e9cc85a374df9e8697ffe
bakhfendi8 <bakh.fendi@gmail.com>
2026-04-22 12:43:06 +0800

    update add, remove function for cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,10 +2,46 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class CartController extends Controller
 {
-    //
+    public function index()
+    {
+        $cart = session('cart', []);
+        return view('cart.index', compact('cart'));
+    }
+
+    public function add(Request $request): RedirectResponse
+    {
+        $cart = session('cart', []);
+
+        $cart[] = [
+            'name'  => $request->input('product'),
+            'price' => (float) $request->input('price'),
+        ];
+
+        session(['cart' => $cart]);
+
+        return redirect()->route('cart.index')
+            ->with('success', $request->input('product') . ' added to cart.');
+    }
+
+    public function remove(Request $request): RedirectResponse
+    {
+        $cart  = session('cart', []);
+        $index = (int) $request->input('index');
+
+        unset($cart[$index]);
+        session(['cart' => array_values($cart)]);
+
+        return redirect()->route('cart.index');
+    }
+
+    public function clear(): RedirectResponse
+    {
+        session()->forget('cart');
+        return redirect()->route('cart.index');
+    }
 }
